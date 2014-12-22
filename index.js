1f0bbc6829e5688e46102d4a359a87706cd01d10
Bob McWhirter <bob@mcwhirter.org>
2014-12-22 12:06:26 -0500

    Flesh out the package.js, add grunt support and basica docs.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
+// # keycloak-authenticated-client
+//
+// This module provides a way to perform authenticated requests against
+// a server which is secured by Keycloak.
+//
+// This client can either authenticate on a user's behalf, or use a
+// grant obtained through other means.
+//
+// ## Usage
+//
+//     var Client = require('keycloak-authenticated-client')
+//     var client = new Client( { username: 'bob', password: 'tacos' } );
+//
+//     client.request( options, function(response) {
+//       response.on( 'data', ... )
+//     } )
+
 module.exports = require('./authenticated-client');
